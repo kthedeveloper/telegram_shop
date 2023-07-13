08a6e2d5879e6605917c053ae7922861d8bf3704
kthedeveloper <ks.ryabov@gmail.com>
2023-07-13 17:04:28 +0300

    Telegram bot for the local bread shop

---
+++ main.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer(f'{message.from_user.first_name}, добро пожаловать в магазин домашнего хлеба и выпечки!')
+
+
+@dp.message_handler()
+async def answer(message: types.Message):
+    await message.reply(
+        'К сожалению, я Вас не понимаю. Воспользуйтесь командой /help чтобы получить инструкции по использованию бота')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
